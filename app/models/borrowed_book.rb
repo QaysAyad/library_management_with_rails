8b03c100df73d2d7fe32aa8dedea03269adf5c58
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2021-08-10 20:39:50 +0300

    adding late? a bool to know if book is late

--- app/models/borrowed_book.rb
+++ app/models/borrowed_book.rb
@@ -2,4 +2,9 @@ class BorrowedBook < ApplicationRecord
   belongs_to :book
   belongs_to :user
   validates_uniqueness_of :book
+
+  # true if book return date has passed, false otherwise
+  def late?
+   returning_at.past? 
+  end
 end
