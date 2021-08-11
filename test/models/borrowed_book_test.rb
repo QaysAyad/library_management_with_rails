0cc4ef9c6884aadb789b8d415c2737c50997c689
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2021-08-11 23:47:00 +0300

    adding BorrowedBook model tests

--- test/models/borrowed_book_test.rb
+++ test/models/borrowed_book_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class BorrowedBookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fails on duplicate book_id" do
+    borrowed_book = create(:borrowed_book)
+    borrowed_book2 = build(:borrowed_book, book: borrowed_book.book)
+    assert( borrowed_book2.errors[:book] )
+  end
+
+  test "late should be true" do
+    borrowed_book = BorrowedBook.new returning_at: 1.day.ago
+    assert( borrowed_book.late? )
+  end
+  
+  test "late should be false when " do
+    borrowed_book = BorrowedBook.new returning_at: 1.day.from_now
+    assert( !borrowed_book.late? )
+  end
 end
